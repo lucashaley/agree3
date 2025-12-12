42e11b6529a749b3a2c94dc261621437680888d9
Lucas Haley <lucashaley@yahoo.com>
2025-12-11 18:22:31 -0800

    rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,12 +70,12 @@ Rails.application.configure do
   # }
 
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'weagreethat-dev.heroku.com',
-    :authentication => :plain,
+    port: ENV["MAILGUN_SMTP_PORT"],
+    address: ENV["MAILGUN_SMTP_SERVER"],
+    user_name: ENV["MAILGUN_SMTP_LOGIN"],
+    password: ENV["MAILGUN_SMTP_PASSWORD"],
+    domain: "weagreethat-dev.heroku.com",
+    authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
 
